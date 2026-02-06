8258cf24d723986c6b2e45b6d0ac25ecbf82ac3b
KISHAN <kishanragunathan26@gmail.com>
2026-02-06 14:36:29 +0530

    palindrome

--- array-problems/array_palindrome/palindrome.java
+++ array-problems/array_palindrome/palindrome.java
@@ -1,5 +1,20 @@
-package array-problems.array_palindrome;
-
-public class palindrome {
-    
+import java.util.Arrays;
+class palindrome {
+    public static void main(String[] args) {
+      int arr[] ={1,2,3,2,1};
+      int rev[] =new int[arr.length];
+      int j=0;
+      for(int i=arr.length-1;i>=0;i--){
+          rev[j]=arr[i];
+          j++;
+      }
+      if(Arrays.equals(arr, rev)){
+          System.out.println("the given Array is palindrome");
+      }else{
+          System.out.println("the given array is not a palindrome");
+      }
+      System.out.println(Arrays.toString(arr));
+      System.out.println(Arrays.toString(rev));
+      
+   }
 }
