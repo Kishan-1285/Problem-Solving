4dacde686a088667662b31e0290ee7512f66ae82
kishan-1285 <kishanragunathan26@gmail.com>
2026-04-04 10:30:00 +0530

    Add Fibonacci input prompt

--- fibonacii series/fibonacii.java
+++ fibonacii series/fibonacii.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
+
 public class fibonacii {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
         int n = sc.nextInt();
+
         int a = 0;
         int b = 1;
-        for(int i=0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             System.out.print(a + " ");
 
-            int next = a+b;
-            a=b;
-            b=next;
+            int next = a + b;
+            a = b;
+            b = next;
         }
         sc.close();
-
     }
 }
