87ed681f30204690488d39642fb6c30a6062258b
kishan-1285 <kishanragunathan26@gmail.com>
2026-04-08 10:00:00 +0530

    Your commit message

---
+++ count_digits/count.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int count = 0;
+        while(a>0){
+            count++;
+            a=a/10;
+        }
+        System.out.println(count);
+    }
+}
