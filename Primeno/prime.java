c982462935f6e97667f2952615ba73161fea3f63
kishan-1285 <kishanragunathan26@gmail.com>
2026-04-15 10:00:00 +0530

    Format prime number program

--- Primeno/prime.java
+++ Primeno/prime.java
@@ -1,20 +1,23 @@
 package Primeno;
+
 import java.util.Scanner;
-public class prime{
-    public static boolean isPrime(int n){
-        if(n<=1){
+
+public class prime {
+    public static boolean isPrime(int n) {
+        if(n <= 1) {
             return false;
         }
 
         // Check divisors only up to the square root of n.
-        for(int i = 2; i*i<=n;i++){
-            if(n%i==0){
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
                 return false;
             }
         }
         return true;
     }
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
@@ -35,5 +38,5 @@ public class prime{
         }
        
         sc.close();
-     }
+    }
 }
