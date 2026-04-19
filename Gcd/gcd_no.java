cdf316051a4b4b96350fe356921e88ac12dcb9c0
kishan-1285 <kishanragunathan26@gmail.com>
2026-04-19 10:00:00 +0530

    Add GCD program

---
+++ Gcd/gcd_no.java
@@ -0,0 +1,34 @@
+package Gcd;
+
+import java.util.Scanner;
+
+public class gcd_no{
+
+    public static int gcd(int a, int b) {
+
+        while(b != 0) {
+
+            int temp = b;
+
+            b = a % b;
+
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("GCD = " + gcd(a, b));
+        sc.close();
+    }
+}
