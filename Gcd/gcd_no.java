a0cf1483bee9e74ed3602ec12a3022ff1f39d846
kishan-1285 <kishanragunathan26@gmail.com>
2026-05-02 10:00:00 +0530

    lcm of 2 nos

--- Gcd/gcd_no.java
+++ Gcd/gcd_no.java
@@ -1,36 +1,23 @@
 package Gcd;
-
 import java.util.Scanner;
-
 public class gcd_no{
-
     public static int gcd(int a, int b) {
         a = Math.abs(a);
         b = Math.abs(b);
-
         // Euclidean algorithm: replace the larger problem with the remainder.
         while(b != 0) {
-
             int temp = b;
-
             b = a % b;
-
             a = temp;
         }
-
         return a;
     }
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
-
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
-
         System.out.println("GCD = " + gcd(a, b));
         sc.close();
     }
---
+++ Lcm/lcm_no.java
@@ -0,0 +1,24 @@
+package Lcm;
+
+import java.util.Scanner;
+public class lcm_no{
+    public static int gcd(int a, int b){
+        while(b!=0){
+          int temp = b;
+          b = a % b;
+          a = temp;
+        }
+        return a;
+    }
+    public static int lcm(int a , int b){
+        return((a*b)/gcd(a,b));
+    }
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("LCM = " + lcm(a, b));
+    }
+}
