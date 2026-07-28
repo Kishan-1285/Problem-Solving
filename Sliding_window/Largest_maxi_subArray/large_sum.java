3eea8b7761c1b7a1f2d81c130604f9621d4d54cf
kishan-1285 <kishanragunathan26@gmail.com>
2026-07-28 22:41:09 +0530

    largest maximum sum in the array

---
+++ Sliding_window/Largest_maxi_subArray/large_sum.java
@@ -0,0 +1,21 @@
+package Sliding_window.Largest_maxi_subArray;
+
+class Main {
+    public static void main(String[] args) {
+        int k = 3;
+        int windowSum = 0;
+        int [] arr = {1,2,3,4,5,6,7};
+        for(int i=0;i<k;i++){
+            windowSum += arr[i];
+       }
+        int largest = windowSum;
+        // System.out.println(windowSum);
+        for(int i=0;i+k<arr.length;i++){
+            windowSum = windowSum - arr[i] + arr[i+k];
+            if(windowSum>largest){
+                largest = windowSum;
+           }
+        }
+       System.out.println(largest);
+    }
+}
