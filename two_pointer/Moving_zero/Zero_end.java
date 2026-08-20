732f7373968d8f5e83fb7a5ecb122c3f9696f653
kishan-1285 <kishanragunathan26@gmail.com>
2026-08-20 15:41:30 +0530

    two pointer

--- two_pointer/Moving_zero/Zero_end.java
+++ two_pointer/Moving_zero/Zero_end.java
@@ -4,24 +4,18 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        int []arr = {1,2,0,0,4,3,0};       
+        int[] arr = { 0, 1, 0, 3, 12 };
         int slow = 0;
         int fast = 0;
-        while(fast<arr.length){
-            if(arr[fast]!=0&&arr[slow]!=0){
-                fast++;
+        while (fast < arr.length) {
+            if (arr[fast] != 0) {
+                int temp = arr[slow];
+                arr[slow] = arr[fast];
+                arr[fast] = temp;
+
                 slow++;
             }
-            else if(arr[fast]==0&&arr[slow]==0){
-                fast++;
-            }
-            else if(arr[slow]==0&&arr[fast]!=0){
-                    int temp = arr[fast];
-                    arr[fast] = arr[slow];
-                    arr[slow] = temp;
-                    fast++;
-                    slow++;
-            }
+            fast++;
         }
         System.out.println(Arrays.toString(arr));
     }
