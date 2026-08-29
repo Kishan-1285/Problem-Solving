327cbb26a8b92d910caccd051c460648e3eaa725
kishan-1285 <kishanragunathan26@gmail.com>
2026-08-29 11:54:04 +0530

    finding the vowels in the string

---
+++ String/vowelsInString/vowels.java
@@ -0,0 +1,17 @@
+
+// Run command: javac vowels.java && java String.vowelsInString.vowels
+package String.vowelsInString;
+
+class vowels {
+    public static void main(String[] args) {
+        String s = "Kishan";
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
